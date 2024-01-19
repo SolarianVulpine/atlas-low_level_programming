d388fe4d017f5526fed66addbd862ab621559303
solarianVulpine <victoria.teale222@gmail.com>
2024-01-19 19:33:32 +0000

    added /n for new line

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
- * main - Entry point
+ * main - use printf to print a exact string
  *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 	return (0);
 }
