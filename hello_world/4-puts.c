1a4fa106462e6a35d3c69cb4b3ef7e7bfc34aa71
solarianVulpine <victoria.teale222@gmail.com>
2024-01-19 18:51:46 +0000

    ben helped

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,7 @@
-// C program to show the use of fputs
+
 #include <stdio.h>
 int main()
 {
-    puts("Programming is like building a mulilingual puzzle"); 
+    puts("\"Programming is like building a mulilingual puzzle"); 
     return 0;
 }
