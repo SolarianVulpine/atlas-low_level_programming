96b931f347a6a8a4029d3e62c3b81e87eadfbb9c
solarianVulpine <victoria.teale222@gmail.com>
2024-01-22 22:09:39 +0000

    2

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -9,11 +9,15 @@
 int print_alphabet(void)
 {
 	char i;
+	int j;
 
-	for (i = 'a'; i <= 'z'; i++)
+	for (j = 0; j < 10; j++) 
+	{
+	for (i = 'a'; i <= 'z'; i++) 
+	{
 		_putchar(i);
-
+	}
 	_putchar('\n');
-
+	}
 	return (0);
 }
