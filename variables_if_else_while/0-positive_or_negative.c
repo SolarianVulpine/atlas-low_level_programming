8d2ae1a2369bef001048c2e11756c31e5b03cd3a
solarianVulpine <victoria.teale222@gmail.com>
2024-01-22 01:18:57 +0000

    pos or neg reandom if printf

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - entry point
+ *
+ * Return: Always return 0
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*if n is pos*/
+	if (n > 0) {
+		printf("%d is positive\n",n);
+	}
+	/*if n is 0*/
+	else if (n == 0) {
+		printf("%d is 0\n",n);
+	}
+	/*if n is neg*/
+	else if (n < 0) {
+		printf("%d is negative\n",n);
+	}
+	return (0);
+}
