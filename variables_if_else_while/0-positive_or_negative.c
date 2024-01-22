36021155b329caa36e4aa6eb459d1c1a89f3d0eb
solarianVulpine <victoria.teale222@gmail.com>
2024-01-22 01:31:19 +0000

    fix betty warnings

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -10,21 +10,20 @@
 
 int main(void)
 {
-        int n;
-
-        srand(time(0));
-        n = rand() - RAND_MAX / 2;
-        /*if n is pos*/
-        if (n > 0) {
-                printf("%d is positive\n",n);
-        }
-        /*if n is 0*/
-        else if (n == 0) {
-                printf("%d is zero\n",n);
-        }
-        /*if n is neg*/
-        else {
-                printf("%d is negative\n",n);
-        }
-        return (0);
+	int n;
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	/*if n is pos*/
+	if (n > 0) {
+		printf("%d is positive\n",n);
+	}
+	/*if n is 0*/
+	else if (n == 0) {
+		printf("%d is zero\n",n);
+	}
+	/*if n is neg*/
+	else {
+		printf("%d is negative\n",n);
+	}
+	return (0);
 }
