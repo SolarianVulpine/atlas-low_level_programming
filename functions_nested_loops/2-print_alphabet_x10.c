8e2a58e123c0b9338fb33f8ba2d412c3e2bec4b2
solarianVulpine <victoria.teale222@gmail.com>
2024-01-22 23:43:57 +0000

    Jman help

--- functions_nested_loops/2-print_alphabet_x10.c
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,26 @@
  */
 void print_alphabet_x10(void)
 {
-	char letter;
 	int i;
 
 	for (i = 0; i < 10; i++)
 	{
-		for (letter = 'a'; letter <= 'z'; letter++)
-		{
-			_putchar(letter);
-		}
-
-		_putchar('\n');
+		print_alphabet();
 	}
+}
+
+/**
+ * print_alphabet - prints the alphabet in lowercase
+ *
+ * Return: always 0
+ */
+
+void print_alphabet(void)
+{
+	char i;
+
+	for (i = 'a'; i <= 'z'; i++)
+		_putchar(i);
+
+	_putchar('\n');
 }
--- functions_nested_loops/main.c
+++ functions_nested_loops/main.c
@@ -1,18 +1,12 @@
 #include "main.h"
-#include <unistd.h>
 
 /**
- * _putchar - writes the character c to stdout
- * @c: The character to print
+ * main - check the code.
  *
- * Return: On success 1.
- * On error, -1 is returned, and errno is set appropriately.
+ * Return: Always 0.
  */
-
 int main(void)
 {
-	int _putchar(char c)
-	{
-		return (write(1, &c, 1));
-	}
-}	
+    print_alphabet_x10();
+    return (0);
+}
