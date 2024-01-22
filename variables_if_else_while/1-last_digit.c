d193db2592821f92b89544166924bda4e001c936
solarianVulpine <victoria.teale222@gmail.com>
2024-01-22 02:14:48 +0000

    biblically accurate code

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -19,7 +19,7 @@ int main(void)
 
 	if (lastn > 5)
 	{
-		printf("The last digit of %d is %d\n", n, n % 10);
+	printf("Last digit of %d is %d and is greater than 5\n", n, lastn);
 	}
 	else if (lastn == 0)
 	{
