19bbdd2f83a3d5ecba0ab2f6b2917dc3f68b748c
solarianVulpine <victoria.teale222@gmail.com>
2024-01-26 05:09:55 +0000

    trying to reverse is lower to check for uppercase

---
+++ more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ * _islower - checks for uppercase character.
+ * @c: The character to be checked.
+ *
+ * Return: 1 if c is uppercase, 0 otherwise.
+ */
+int _isupper(int c)
+{
+	return (c >= 'a' && c <= 'z', % 2);
+}
