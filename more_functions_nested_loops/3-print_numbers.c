fdac80d85bf95fe853a3efa665730fb8c44e3b14
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 00:06:39 +0000

    wrote a for loop code into task 3

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * print_numbers : prints the digits 0 through 9
+ *
+ * Return: Always 0.
+ *
+ */
+
+void print_numbers(void)
+
+{
+	i = int
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar("%d\n", i);
+	}
+	return (0);
+}
