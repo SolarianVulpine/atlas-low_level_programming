f7ed5abf41dd77414201f1d8917d464032fd9be5
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 07:03:27 +0000

    fizzbuzz challenge

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,35 @@
+#include <stdio.h>
+
+/**
+ * fizz_buzz - prints 0-100, multipls of 3 are fizz multipls of 5 are buzz mltpls of both are fizzbuzz
+ *
+ * Return 0
+ */
+
+int fizz_buzz()
+
+{
+	int n;
+
+	for (n = 1; n <= 100; n++);
+	{
+		if (n % 3 == 0 && n % 5 == 0)
+		{
+			printf("FizzBuzz\n");
+		}
+		else if (n % 3 == 0)
+		{
+			printf("Fizz\n");
+		}
+		else if (n % 5 == 0)
+		{
+			printf("Buzz\n");
+		}
+		else
+		{
+			printf("%d\n",n);
+		}
+	}
+
+	Return (0);
+}
