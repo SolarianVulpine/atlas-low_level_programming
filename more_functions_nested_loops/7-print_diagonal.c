64acc2d2c2b3f5bae4b131b1862ee67ffd375a27
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 05:38:43 +0000

    added if statement and escape symbol \

--- more_functions_nested_loops/7-print_diagonal.c
+++ more_functions_nested_loops/7-print_diagonal.c
@@ -9,13 +9,23 @@
 void print_diagonal(int n)
 {
 	int i;
+	int j;
 
 	if (n > 0)
 	{
-		for (i = 1; i <= n; i++)
+		_putchar('\\');
+		_putchar('\n');
+
+		for (i = 1; i <= (n - 1); i++)
 		{
-			_putchar('\');
+			for (j = 1; j <= i; j++)
+			{
+				_putchar(' ');
+			}
 		}
+		_putchar('\\');
+                _putchar('\n');
 	}
-	_putchar('\n');
+	if (n <= 0)
+		_putchar('\n');
 }
