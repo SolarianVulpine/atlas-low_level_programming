3a373cf6b64f75748bc94aebae15df89f484975e
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 07:19:11 +0000

    removed excessive new lines

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -13,11 +13,11 @@ int main(void)
 	for (n = 1; n <= 100; n++)
 	{
 		if ((n % 3 == 0) && (n % 5 == 0))
-			printf("FizzBuzz\n");
+			printf("FizzBuzz");
 		else if (n % 3 == 0)
-			printf("Fizz\n");
+			printf("Fizz");
 		else if (n % 5 == 0)
-			printf("Buzz\n");
+			printf("Buzz");
 		else
 			printf("%d",n);
 		if (n < 100)
