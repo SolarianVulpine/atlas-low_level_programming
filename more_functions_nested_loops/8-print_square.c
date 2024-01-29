50f6fdb3b0e28ced3b4fd67d23e227a87c8f51a4
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 06:22:38 +0000

    using concept from task five to repeat # for n

---
+++ more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * print_square - will print the # symbol to create a square
+ *
+ * @n : length or amount of #
+ *
+ * @i : iterate through rows
+ *
+ * @j : iterate over columns
+ */
+
+void print_square(int size)
+
+{
+	int i;
+	int j;
+	int n;
+
+	for(i = 1; i <= n; i++)
+	{
+		for(j = 1; j <= n; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
