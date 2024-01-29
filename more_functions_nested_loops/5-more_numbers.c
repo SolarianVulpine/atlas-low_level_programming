5b02c3d692d7cab137467e37364e121d57f4db7f
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 03:43:26 +0000

    added if statement

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -10,13 +10,15 @@ void more_numbers(void)
 	int j;
 	int i;
 
-	for (j = 0; j <= 10; j++)
+	for (j = 1; j <= 10; j++)
 	{
 		for (i = 0; i <= 14; i++)
 		{
-			_putchar((i / 10) + '0');
+			if (i >= 10)
+			{
+				_putchar((i / 10) + '0');
+			}
+			_putchar((i % 10) + '0');
 		}
-		_putchar((i % 10) + '0');
-	}
-	_putchar('\n');
+		_putchar('\n');
 }
