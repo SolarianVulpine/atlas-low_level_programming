10cfd87ab4f50177879c6cfb6dee5980aa16b573
solarianVulpine <victoria.teale222@gmail.com>
2024-01-29 17:19:45 +0000

    first draft of triangle

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * print_triangle - will print # in a triangle shape based on size provided
+ *
+ * @n : is the size of the triangle
+ *
+ * i : will be rows
+ * j : will be collumns
+ *
+ * return : nothing
+ */
+
+void print_triangle(int n)
+{
+	int i;
+	int j;
+	
+	{
+		for (i = 1; i <= n; i++)
+		{
+			for (j = 1; j <= n; j++)
+			{
+				if (j <= (n - i))
+					printf(".");
+				else
+					printf("#");
+			}
+			printf("\n");
+		}
+		if (n <= 0)
+			printf("\n");
+	}
+}
