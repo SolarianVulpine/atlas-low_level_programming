5014cee69857a8d6953cd35ae94fbf359fb995c0
solarianVulpine <victoria.teale222@gmail.com>
2024-01-30 05:16:56 +0000

    trailing whitespace

---
+++ pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * puts2 - Prints every other character of the string, starting with the first
+ * @str: Pointer to the input string
+ */ 
+
+void puts2(char *str)
+{
+	unsigned int i = 0;
+	unsigned int limit = 446;
+
+	while (str[i] != '\0' && i < limit)
+	{
+		_putchar(str[i]);
+		i += 2;
+	}
+
+	_putchar('\n');
+}
