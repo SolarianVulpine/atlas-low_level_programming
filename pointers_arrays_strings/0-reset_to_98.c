046dce1c34ed90a3f93b6db864bf46df0a3ff3c2
solarianVulpine <victoria.teale222@gmail.com>
2024-01-30 04:49:18 +0000

    using temp variable

--- pointers_arrays_strings/0-reset_to_98.c
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -1,4 +1,4 @@
-#include "main.h"
+#include <stdio.h>
 
 /**
  * reset_to_98 - resets the variable value of *n to 98
--- pointers_arrays_strings/1-swap.c
+++ pointers_arrays_strings/1-swap.c
@@ -1,1 +1,19 @@
 #include "main.h"
+
+/**
+ * swap_int - will swap two ints
+ *
+ * @a : Pointer to first int
+ *
+ * @b : Pointer to second int
+ */
+
+void swap_int(int *a, int *b)
+{
+	/* will make a temporary variable to swap values */
+	int temp;
+
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
