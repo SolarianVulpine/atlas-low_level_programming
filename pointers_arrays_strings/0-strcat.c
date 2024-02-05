84e9775cb0773081f6f16850105dcee1bb71f6e8
solarianVulpine <victoria.teale222@gmail.com>
2024-02-05 19:41:51 +0000

    strcat file test

---
+++ pointers_arrays_strings/0-strcat.c
@@ -0,0 +1,21 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * _strcat - aims to concatenate 2 strings overwriting a null byte to do so
+ *
+ * @dest : the destination string 1
+ * @src : the source string 2 which will appended to string 1
+ *
+ * Return : dest the destination pointer
+ */
+
+char *_strcat(char *dest, char *src);
+{
+	_strcat(*dest, *src);
+
+	printf("%c", *dest);
+	printf("%c", *src);
+
+	return (*dest)
+}
---
+++ pointers_arrays_strings/main.h
@@ -0,0 +1,6 @@
+#ifdef MAIN_H
+#define MAIN_H
+
+char *_strcat(char *dest, char *src);
+
+#endif /*MAIN_H*/
