62a0a94cb5cbabc9ea1b4707d0e411bfa7d3deab
solarianVulpine <victoria.teale222@gmail.com>
2024-02-05 23:21:57 +0000

    saving work on strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -2,4 +2,36 @@
 #include <string.h>
 #include <stdio.h>
 
+/**
+ * _strncpy - aims to copy a string or mimic strncpy
+ *
+ * @dest : the destination string
+ * @src : the source string to be copied
+ * @n : the number of characters to copy
+ *
+ * Return: dest pointer to destination
+ */
 
+char *_strncpy(char *dest, char *src, int n)
+{
+	if (dest == NULL || src == NULL || n == 0)
+	{
+		return dest;
+	}
+
+	int i = 0;
+
+	while (i < n && src[i] != '\0')
+	{
+		dest[i] = src[i];
+		i++;
+	}
+
+	while (i < n)
+	{
+		dest[i] = '\0';
+		i++;
+	}
+
+	return dest;
+}
