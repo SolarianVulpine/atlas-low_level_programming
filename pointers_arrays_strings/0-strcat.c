671623c9aa3f867eeef94bdb0e41196e549e3a6b
solarianVulpine <victoria.teale222@gmail.com>
2024-02-05 20:32:14 +0000

    removed space in description of retur

--- pointers_arrays_strings/0-strcat.c
+++ pointers_arrays_strings/0-strcat.c
@@ -2,12 +2,12 @@
 #include <string.h>
 
 /**
- * *_strcat - aims to concatenate 2 strings overwriting a null byte to do so
+ * _strcat - aims to concatenate 2 strings overwriting a null byte to do so
  *
  * @dest : the destination string 1
  * @src : the source string 2 which will appended to string 1
  *
- * Return : dest the destination pointer
+ * Return: dest the destination pointer
  */
 
 char *_strcat(char *dest, char *src)
