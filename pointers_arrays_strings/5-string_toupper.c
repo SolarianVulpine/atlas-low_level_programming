fb9ced0d039d0c52ba29112dfd608cfe287d6cd2
solarianVulpine <victoria.teale222@gmail.com>
2024-02-06 03:08:09 +0000

    betty fixes

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -16,11 +16,11 @@ char *string_toupper(char *s)
 	int i;
 	
 	for (i = 0; s[i] != '\0'; i++)
+	{
+		if ((s[i] >= 96) && (s[i] <= 122))
 		{
-			if ((s[i] >= 96) && (s[i] <= 122))
-			{
-				s[i] = s[i] - 32;
-			}
+			s[i] = s[i] - 32;
 		}
+	}
 	return (s);
 }
