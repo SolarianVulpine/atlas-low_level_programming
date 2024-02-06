e4fa44aa7a5d37ac4c9c88c9c9017ec72ae3bf42
solarianvulpine <victoria.teale222@gmail.com>
2024-02-06 23:11:13 +0000

    for loop to determine up to n then assigned a variable to the value of pointer s to be reassigned so the value equals b

---
+++ pointers_arrays_strings/0-memset.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_memset - aims to fill the first 'n' bytes of memory with the constant 'b'
+ *
+ * @s: pointer to memory space
+ * @b: the value to be assigned
+ * @n: the number of bytes to be filled starting at s
+ *
+ * Return: pointer to the memory area in s
+ */
+
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (*s);
+}
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -10,4 +10,6 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 
+char *_memset(char *s, char b, unsigned int n);
+
 #endif /*MAIN_H*/
