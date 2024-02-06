a2e7c2fd71616f52a76787a5bf04a31331662b6b
solarianVulpine <victoria.teale222@gmail.com>
2024-02-06 00:26:38 +0000

    switched around s1 and s2 return operations

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -21,11 +21,11 @@ int _strcmp(char *s1, char *s2)
 	}
        if (s1[i] < s2[i])
 	{
-		return (s1[i] - s2[i]);
+		return (s2[i] - s1[i]);
 	}
        else if (s1[i] > s2[i])
 	{
-		return (s2[i] - s1[i]);
+		return (s1[i] - s2[i]);
 	}
        else
        {
