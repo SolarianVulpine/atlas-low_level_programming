3b97e42ab54a092143c2fc3329f398f3827405c5
solarianVulpine <victoria.teale222@gmail.com>
2024-02-06 00:39:45 +0000

    betty repairs

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -15,20 +15,20 @@ int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
 
-	while(s1[i] == s2[i] && s1[i] != '\0')
+	while (s1[i] == s2[i] && s1[i] != '\0')
 	{
 		i++;
 	}
-       if (s1[i] < s2[i])
+if (s1[i] < s2[i])
 	{
-		return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 	}
-       else if (s1[i] > s2[i])
+else if (s1[i] > s2[i])
 	{
-		return (s1[i] - s2[i]);
+	return (s1[i] - s2[i]);
 	}
-       else
-       {
-	       return (0);
-       }
+else
+{
+	return (0);
+}
 }
