52677d8b9a45eca8f8afdd2948adf88ec5b2807a
solarianVulpine <victoria.teale222@gmail.com>
2024-02-06 02:56:08 +0000

    gave the function an if statement limitting for loop to lowercase letters

--- pointers_arrays_strings/5-string_toupper.c
+++ pointers_arrays_strings/5-string_toupper.c
@@ -15,9 +15,12 @@ char *string_toupper(char *s)
 {
 	int i;
 	
-	for (i = 0; s[i] != '\0'; i++)
+	if (i <= 97 && <= 122)
 	{
-		s[i] = s[i] - 32;
+		for (i = 0; s[i] != '\0'; i++)
+		{
+			s[i] = s[i] - 32;
+		}
 	}
 	return (s);
 }
