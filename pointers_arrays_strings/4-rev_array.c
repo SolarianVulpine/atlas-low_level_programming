bfe4ab4b9295908a031a42cf4e785aaa8256fe60
solarianVulpine <victoria.teale222@gmail.com>
2024-02-06 02:27:14 +0000

    reversing array integers by going around in circles

---
+++ pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * reverse_array - aims to reverse the order of integers within an array
+ *
+ * @a: the array of integers
+ * @n: the number of elements to swap
+ *
+ * Return: void
+ */
+
+void reverse_array(int *a, int n)
+{
+	int temp, j;
+	j = 0;
+
+	while (j < n)
+	{
+		temp = a[n];
+		a[n] = a[j];
+		a[j] = temp;
+		j++;
+		n--;
+	}
+}
