5093e36e64b4ce3bb8db0fd52b7cb31ae53e3ed5
solarianvulpine <victoria.teale222@gmail.com>
2024-02-12 02:29:49 +0000

    saving work

---
+++ 0-puts_recursion.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _puts_recursion - aims to use recursion to print a string then a new line
+ *
+ *@s: the string to be printed
+ *
+ * Return: 0 on success
+ */
+
+void _puts_recursion(char *s)
+{
+
