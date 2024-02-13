4d93c9153a517b2f1076c9eb64cf57bc578ab0cb
solarianvulpine <victoria.teale222@gmail.com>
2024-02-13 17:00:02 +0000

    saving work on 4

---
+++ recursion/4-pow_recursion.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+
+/*
+ * pow_recursion- the function to recursively find x^y
+ *
+ * @x: the numeral to be provided
+ * @y: the exponent numeral
+ */
+
+int _pow_recursion(int x, int y)
+{
+	if (y < 0)
+	{
+		return (-1);
+	}
+	else if (y == 0)
+	{
+		return (0)'
+	}
+	else
+	{
+		return (x
