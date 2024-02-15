f6db8928447601073f3ff49d5d1687cfd8938802
solarianvulpine <victoria.teale222@gmail.com>
2024-02-15 01:16:43 +0000

    saving work on prime task

---
+++ recursion/6-is_prime_number.c
@@ -0,0 +1,12 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdbool.h>
+
+/**
+ * primechecker - checks if a number is prime through division
+ * @n: the provided numeral
+ * @i: a variable to assist with checking prime status
+ * Return: determined by function
+ */
+
+
