980879d46f346dd2ae168fd7afc76bed97b2acac
solarianvulpine <victoria.teale222@gmail.com>
2024-02-15 00:58:44 +0000

    betty and compile fixes task 5 returned expected results

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -3,32 +3,32 @@
 
 /**
  * sqrt_finder - aims to frind the square root of a numeral
- * @n : the provided numeral
- * @i : variable used to check if natural sqrt is possible
+ * @n: the provided numeral
+ * @j: variable used to check if natural sqrt is possible
  *
- * Return : as defined by the function
+ * Return: as defined by the function
  */
 
 int sqrt_finder(int n, int j)
 {
-	if ((j ^ 2) == n)
+	if ((j * j) == n)
 	{
 		return (j);
 	}
-	else if ((j ^ 2) > n)
+	else if ((j * j) > n)
 	{
 		return (-1);
 	}
 	else
 	{
-		return (sqrt_finder(n,(j + 1)));
+		return (sqrt_finder(n, (j + 1)));
 	}
 }
 
 /**
  * _sqrt_recursion - uses sqrt_finder to return the result
- * @n : the provided numeral
- * Return : square root if natural, -1 if not possible
+ * @n: the provided numeral
+ * Return: square root if natural, -1 if not possible
  */
 
 int _sqrt_recursion(int n)
