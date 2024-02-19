2ce3803e3e1b77dd90bd43217b6dccc25419262f
solarianvulpine <victoria.teale222@gmail.com>
2024-02-19 21:36:29 +0000

    saving work on argcargv project

---
+++ argc_argv/1-args.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - aims to print the number of arguments minus the program name
+ * @argc: int to determine num of arguments
+ * @argv: array of char pointers to the arguments
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+	{
+		printf("%d\n", argc -  1);
+	}
+	return  0;
+}
