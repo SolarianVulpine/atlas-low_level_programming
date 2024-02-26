c8c42b063e3268becff8d8ac9f202f0ec121db43
solarianvulpine <victoria.teale222@gmail.com>
2024-02-26 01:47:29 +0000

    4-add

---
+++ argc_argv/4-add.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * add - aims to print sum of positive numbers and a new line
+ * @argc: The number of arguments passed to the program
+ * @argv: An array of pointers to the arguments
+ *
+ * Return: 1 if provided values are non-digits Otherwise return 0
+ */
+
+int add(int argc, char *argv[])
+{
+	int n, digit, sum = 0;
+
+	for (n = 1; num < argc; n++)
+	{
+		for (digit = 0; argv[n][digit]; digit++)
+		{
+			if (argv[n][digit] < '0' || argv[n][digit] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+
+		sum += atoi(argv[n]);
+	}
+
+	printf("%d\n", sum);
+
+	return (0);
+}
