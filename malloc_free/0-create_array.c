97c04c73c56a47f0f0195cc700f01c4e1e3f134e
solarianvulpine <victoria.teale222@gmail.com>
2024-02-27 03:31:51 +0000

    major compiler errors but im tired so ill tackle it tomorrow

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int i;
+	char *arr
+
+	if (size == 0)
+	{
+		return (NULL);
+	}
+	
+	char* arr  (char*)malloc(size * sizeof(char));
+
+	if (arr == NULL)
+	{
+		return (NULL);
+	}
+	for (i = 0; i < size; i++)
+	{
+		arr[i] = c;
+	}
+	
+	return (arr);
+}
