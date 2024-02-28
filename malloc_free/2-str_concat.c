e18821c620809cc4339337d238f0ccf2843a342a
solarianvulpine <victoria.teale222@gmail.com>
2024-02-28 03:21:41 +0000

    jeremy call function method

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -22,12 +22,12 @@ char *str_concat(char *s1, char *s2)
 	if (s1 == NULL)
 		len1 = 0;
 	else
-		len1 = _strlen(s1);
+		len1 = strlen(s1);
 
 	if (s2 == NULL)
 		len2 = 0;
 	else
-		len2 = _strlen(s2);
+		len2 = strlen(s2);
 
 	res = malloc(sizeof(char) * (len1 + len2 + 1));
 
