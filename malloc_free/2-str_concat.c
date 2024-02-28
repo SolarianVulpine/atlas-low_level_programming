55b943c851e58b7bff4d5289f566d21620ef792e
solarianvulpine <victoria.teale222@gmail.com>
2024-02-28 03:03:06 +0000

    assisted result

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -1,38 +1,50 @@
+#include "main.h"
 #include <stdlib.h>
 #include <string.h>
 
 /**
- * str_concat - Concatenates two strings
+ * str_concat - concatenates two strings
+ * @s1: first string to concatenate
+ * @s2: second string to concatenate
  *
- * @s1: string
- * @s2: 2nd string
- *
- * Return: pointer to new allocated concatenated string,
- *         or NULL
+ * Return: concatenated string, NULL on failure
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	char *cat;
-	size_t len_s1 = (s1 == NULL) ? 0 : strlen(s1);
-	size_t len_s2 = (s2 == NULL) ? 0 : strlen(s2);
+	char *catStr;
+	int i;
+	int j;
+	int lenS1;
+	int lenS2;
 
-	cat = (char *)malloc((len_s1 + len_s2 + 1) * sizeof(char));
+	if (s1 == NULL)
+		s1 = "";
 
-	if (cat == NULL)
+	if (s2 == NULL)
+		s2 = "";
+
+	lenS1 = strlen(s1);
+	lenS2 = strlen(s2);
+
+	catStr = malloc(sizeof(char) * (lenS1 + lenS2) + 1);
+
+	if (catStr == NULL)
 	{
-		return (NULL); 
+		return (NULL);
 	}
 
-	if (s1 != NULL)
+	for (i = 0; i < lenS1; i++)
 	{
-		strcpy(cat, s1);
+		catStr[i] = s1[i];
 	}
 
-	if (s2 != NULL)
+	for (j = 0; j < lenS2; j++, i++)
 	{
-		strcat(cat, s2);
+		catStr[i] = s2[j];
 	}
 
-	return (cat);
+	catStr[i] = '\0';
+
+	return (catStr);
 }
