cceae42b887c78220ea5de5449c59659d7938e4e
solarianvulpine <victoria.teale222@gmail.com>
2024-05-16 22:11:59 +0000

    ran compiler locally and found typo, very cool

---
+++ hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Description: this program practices the printf function
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
