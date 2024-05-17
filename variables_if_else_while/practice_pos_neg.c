5efd42f9a5662f32580bb57917ec80c143729c50
solarianvulpine <victoria.teale222@gmail.com>
2024-05-17 03:55:34 +0000

    changed 0-positive_or_negative file name to practice_pos_neg, made it take user input

--- variables_if_else_while/practice_pos_neg.c
+++ variables_if_else_while/practice_pos_neg.c
@@ -11,9 +11,9 @@ int main(void)
 {
 	int n;
 
-	srand(time(0));
-	n = rand() - RAND_MAX / 2;
-	/*your code goes there*/
+	printf("Please enter a number\n");
+	scanf("%d", &n);
+
 	if (n > 0)
 	{
 		printf("%d is positive\n", n);
