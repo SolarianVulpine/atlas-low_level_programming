f69537faad06a33694795171f17a0e4211500d82
solarianvulpine <victoria.teale222@gmail.com>
2024-05-18 03:02:28 +0000

    saving work

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - entry point of my function
+ *
+ * Description: This code prints the alphabet using putchar
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int i;
+	char 
