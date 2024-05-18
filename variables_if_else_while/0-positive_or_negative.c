09f1008156ad7643db90d1a69b4b0cbf55ca399f
solarianvulpine <victoria.teale222@gmail.com>
2024-05-18 00:32:03 +0000

    added stdlib.h library and compiled

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,31 @@
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+/**
+ * main - entry point
+ *
+ * Description: this code should determine whether n is pos or neg
+ *
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+
+	if (n > 0)
+	{
+		printf("%d is positive\n", n);
+	}
+	else if (n < 0)
+	{
+		printf("%d is negative\n", n);
+	}
+	else if (n == 0)
+	{
+		printf("%d is zero\n", n);
+	}
+	return (0);
+}
