9c1ae7095b0a8428f6a750a0a69b70237ad1664f
solarianvulpine <victoria.teale222@gmail.com>
2024-05-19 02:01:35 +0000

    used ascii key to loop putchar through the alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+/**
+ * main - entry point of my function
+ *
+ * Description: This code prints the alphabet using putchar
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+
+	{
+		for (i = 97; i <= 122; i++)
+		{
+			putchar(i);
+		}
+		putchar('\n');
+	}
+	return (0);
+}
