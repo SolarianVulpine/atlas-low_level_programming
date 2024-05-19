bbbca24aa140d1f8d91bfde4350276319941df36
solarianvulpine <victoria.teale222@gmail.com>
2024-05-19 00:52:58 +0000

    betty'd  and compiled with no errors time for the checker

--- variables_if_else_while/3-print_alphabets.c
+++ variables_if_else_while/3-print_alphabets.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+
 /**
  * main - entry point of my function
  *
@@ -6,20 +7,21 @@
  *
  * Return: Always 0 (success)
  */
+
 int main(void)
 {
 	int i;
 	{
-		for(i = 97; i <= 122; i++)
+		for (i = 97; i <= 122; i++)
 		{
 			putchar(i);
 		}
-		for(i = 65; i <= 90; i++)
+		for (i = 65; i <= 90; i++)
 		{
 			putchar(i);
 		}
 	}
-	if(i == 90)
+	if (i == 90)
 	{
 		putchar('\n');
 	}
