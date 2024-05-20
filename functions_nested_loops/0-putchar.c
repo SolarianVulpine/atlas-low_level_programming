4be37780cabe93734e0b90f758e339c7ebe16e95
solarianvulpine <victoria.teale222@gmail.com>
2024-05-20 01:58:45 +0000

    created directory, readme, copied _putchar.c, added and coded task 0

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/*
+ * main - the entry point to my code
+ *
+ * Description: this code should print '_putchar' followed by a new line
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	printf("_putchar\n");
+
+	return (0);
+
+}
