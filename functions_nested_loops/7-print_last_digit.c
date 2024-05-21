ad1cf55af9147f11a8803f29de73f841c94b1f44
solarianvulpine <victoria.teale222@gmail.com>
2024-05-21 00:36:55 +0000

    taking this code in task 7 to python tutor for analysis

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * print_last_digit - main entry point of the program
+ *
+ * Description: this code should print the last digit of a provided number
+ *
+ * @i: the number to be checked
+ *
+ * Return: the value of the last digit
+ */
+
+int print_last_digit(int i)
+{
+	int j;
+
+	j = (i % 10);
+
+	_putchar(j);
+
+	return (i % 10);
+}
