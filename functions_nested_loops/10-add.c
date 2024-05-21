0ab9220f64d781e51aab0c7221d5ef5ee7b6696f
solarianvulpine <victoria.teale222@gmail.com>
2024-05-21 04:24:41 +0000

    this task was wildly simple compared to the last one

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * add - main entry point
+ *
+ * Description: adds integers
+ *
+ * @a: first number
+ * @b: second number
+ *
+ * Return: the result of the operation a + b
+ */
+
+int add(int a, int b)
+{
+	return (a + b);
+}
