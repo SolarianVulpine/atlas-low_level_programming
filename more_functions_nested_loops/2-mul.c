0ab254753610dec615a60a7364f83eac90f1e906
solarianvulpine <victoria.teale222@gmail.com>
2024-05-24 23:15:11 +0000

    defined int result as a * b

---
+++ more_functions_nested_loops/2-mul.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * mul - the entry point of my code
+ *
+ * Description: Should multiply two integers
+ *
+ * @a: first integer
+ *
+ * @b: second integer
+ *
+ * Return: will return 0 upon success
+ */
+
+int mul(int a, int b)
+{
+	int result = a * b;
+
+	return (result);
+}
