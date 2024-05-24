27f0c087527d52e935499f48f5fcad22704947a8
solarianvulpine <victoria.teale222@gmail.com>
2024-05-24 23:52:48 +0000

    made outter loop count to < 10 instead of 11, removed trailing whitespace

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -3,7 +3,7 @@
 /**
  * more_numbers - the entry point of my code
  *
-14 * Description: will print the digits 0-9 and a \n 
+14 * Description: will print the digits 0-9 and a \n
 using putchar thrice
  *
  * Return: will return 0 on success
