cfb6689c4876b05b6783c70c7bc35717e070e099
solarianvulpine <victoria.teale222@gmail.com>
2024-05-26 18:17:39 +0000

    created 2-main.c and 2-strlen.c

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * _strlen - will return the string length
+ *
+ * Description:
+ *
+ * @s: the provided string
+ *
+ * Return: the length of the string
+ */
+
+int _strlen(char *s)
+{
+
