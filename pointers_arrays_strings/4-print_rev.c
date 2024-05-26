600ea77e5d07918f72927ee82b28441afdd39fb8
solarianvulpine <victoria.teale222@gmail.com>
2024-05-26 20:49:04 +0000

    saving work on string reverse and taking a break

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_rev - will print a string backwards
+ *
+ * Description:
+ *
+ * @s: the provided string
+ *
+ * Return: 0 on success
+ */
+
+void print_rev(char *s)
+{
+	int i, length = 0;
+
+	while (s[length !=
