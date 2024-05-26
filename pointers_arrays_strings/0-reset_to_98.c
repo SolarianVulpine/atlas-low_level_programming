ec54d1550fce608308e3126bf526e44d7b28038d
solarianvulpine <victoria.teale222@gmail.com>
2024-05-26 00:22:07 +0000

    initiallizes variable and assigns a value to pointer using that variable

---
+++ pointers_arrays_strings/0-reset_to_98.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * reset_to_98 - this code will update a pointer's value
+ *
+ * Description: will use declarations to change a provided value
+ * to equal 98 regardless of original value
+ *
+ * @n: this is the provided pointer
+ *
+ * Return: Always 0 on success
+ */
+
+void reset_to_98(int *n)
+{
+	int i = 98;
+	*n = i;
+}
+
