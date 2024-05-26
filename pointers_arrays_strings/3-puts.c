a19533e849349607084ff1fd9a69fefc50140163
solarianvulpine <victoria.teale222@gmail.com>
2024-05-26 17:55:18 +0000

    used an array to iterate through the values of a string

--- pointers_arrays_strings/3-puts.c
+++ pointers_arrays_strings/3-puts.c
@@ -1,4 +1,25 @@
+#include <stdio.h>
 #include "main.h"
 
 /**
- * _puts - aims to print a line 
+ * _puts - aims to print a string followed by a new line
+ *
+ * Description: utilizes an array loop to identify the value of
+ * each character space. then plugs that value into putchar
+ *
+ * @str: the provided string
+ *
+ * Return: 0 upon success
+ */
+
+void _puts(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+
+}
