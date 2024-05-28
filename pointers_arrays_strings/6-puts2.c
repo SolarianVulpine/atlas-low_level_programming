4e0d7116c638505d6c87aea513487e31d6b54d88
solarianvulpine <victoria.teale222@gmail.com>
2024-05-28 00:42:50 +0000

    altered for loop in puts code to count up by 2s instead of 1s making it print every other character

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -1,2 +1,24 @@
 #include "main.h"
 
+/**
+ * puts2 - this program should print every other character
+ *
+ * Description: using the for loop we iterate through the string
+ * using += 2 we print every other character by counting up by
+ * 2 instead of by 1
+ *
+ * @str: the provided string
+ *
+ * Return: 0 upon success
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; str[i] != '\0'; i += 2)
+	{
+		_putchar(str[i]);
+	}
+	_putchar('\n');
+}
