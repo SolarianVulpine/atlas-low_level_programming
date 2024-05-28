dfb531fb5ab2fd3ed05b66240155686d00e84242
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-05-28 22:57:09 +0000

    tested 6-puts2 in github coding spaces and began work on atoi

---
+++ pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+ * _atoi - converts a string to an integer
+ * 
+ * Description:
+ * 
+ * @s: the provided string
+ * 
+ * Return: 0 upon success
+*/
+
+int _atoi(char *s)
+{
+    
+}
