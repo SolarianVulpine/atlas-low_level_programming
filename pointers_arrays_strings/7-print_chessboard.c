dedeb68aa0c08a2e1ab7afd61c825a658f4eff67
solarianvulpine <victoria.teale222@gmail.com>
2024-06-11 02:56:27 +0000

    betty style and documentation fixes in chessboard

--- pointers_arrays_strings/7-print_chessboard.c
+++ pointers_arrays_strings/7-print_chessboard.c
@@ -4,6 +4,7 @@
 /**
 * print_chessboard - prints a chessboard pattern
 *
+* @a: 2d array representing the chessboard
 * @grid: 2D array representing the chessboard
 *
 * Return: void
@@ -11,13 +12,14 @@
 
 void print_chessboard(char (*a)[8])
 {
-    int row, col;
-    for (row = 0; row < 8; row++)
-    {
-        for (col = 0; col < 8; col++)
-        {
-            putchar(a[row][col]);
-        }
-        putchar('\n');
-    }
+	int row, col;
+
+	for (row = 0; row < 8; row++)
+	{
+		for (col = 0; col < 8; col++)
+		{
+			putchar(a[row][col]);
+		}
+		putchar('\n');
+	}
 }
