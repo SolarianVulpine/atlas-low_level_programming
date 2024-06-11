575cab692e40278d6a06c1d296b032c125468c00
solarianvulpine <victoria.teale222@gmail.com>
2024-06-11 02:53:26 +0000

    betty fixes in strpbrk

--- pointers_arrays_strings/4-strpbrk.c
+++ pointers_arrays_strings/4-strpbrk.c
@@ -2,28 +2,29 @@
 #include <stddef.h>
 
 /**
- * _strpbrk - gets the length of a prefix substring
- *
- * @s: string
- * @accept: another string
- *
- * Return: pointer or NULL
+* _strpbrk - gets the length of a prefix substring
+*
+* @s: string
+* @accept: another string
+*
+* Return: pointer or NULL
 */
 
 char *_strpbrk(char *s, char *accept)
 {
-    int i;
-    int j;
+	int i;
 
-    for (i = 0; s[i] != '\0'; i++)
-    {
-        for (j = 0; accept[j] != '\0'; j++)
-        {
-            if(s[i] == accept[j])
-            {
-                return (&s[i]);
-            }
-        }
-    }
-    return (NULL);
+	int j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept[j])
+			{
+				return (&s[i]);
+			}
+		}
+	}
+	return (NULL);
 }
