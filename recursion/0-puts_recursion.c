19e5ddbf390a2a68819708aed49f727e41c6fd29
solarianvulpine <victoria.teale222@gmail.com>
2024-06-11 03:11:20 +0000

    rewrote puts recursion for checker

--- recursion/0-puts_recursion.c
+++ recursion/0-puts_recursion.c
@@ -11,12 +11,13 @@
 
 void _puts_recursion(char *s)
 {
-	if (*s != '\0')
-		_putchar('\n');
-	else
+	if (s[0] != '\0')
 	{
-		_putchar(*s);
-		_puts_recursion(s + 1);
+		_putchar(s[0]);
+		s++;
+		_puts_recursion(s);
 	}
+	else
+		_putchar('\n');
 
 }
