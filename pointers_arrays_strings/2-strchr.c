48e4aa125b67c1c272e74023d135ad1476ae7300
solarianvulpine <victoria.teale222@gmail.com>
2024-06-11 02:10:31 +0000

    betty fixes and checker run on strchr, rearranged loops and if statements to form better logic

--- pointers_arrays_strings/2-strchr.c
+++ pointers_arrays_strings/2-strchr.c
@@ -2,26 +2,26 @@
 #include <stddef.h>
 
 /**
- * _strchr - locates a char in a string
- *
- * @c: the character
- * @s: the string
- *
- * Return: s, '\0'
- */
+* _strchr - locates a char in a string
+*
+* @c: the character
+* @s: the string
+*
+* Return: s, '\0'
+*/
 
- char *_strchr(char *s, char c)
+char *_strchr(char *s, char c)
 {
-	while (*s != '\0')
+	while (*s)
 	{
-		if (*s == c)
-		{
+		if ((*s) == c)
 			return (s);
-		}
 		s++;
 	}
+
 	if ((*s) == c)
 		return (s);
 	else
 		return (NULL);
+
 }
