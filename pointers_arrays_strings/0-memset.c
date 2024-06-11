43dff60e80725be96cd082d66a0b08e3a5ac4688
solarianvulpine <victoria.teale222@gmail.com>
2024-06-11 02:03:21 +0000

    betty fixes and checker run on memset

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -1,6 +1,7 @@
 #include "main.h"
 
-/** _memset - this code will fill memory with a constant byte
+/**
+* _memset - this code will fill memory with a constant byte
 *Description:
 *
 * @n: this is the provided amount of bytes
@@ -8,15 +9,15 @@
 * @s: the pointer to the memory location
 *
 * Return: A pointer to memory area "s"
-*/ 
+*/
 
 char *_memset(char *s, char b, unsigned int n)
 {
-    unsigned int i;
+	unsigned int i;
 
-    for (i = 0; i <= n; i++)
-    {
-        s[i] = b;
-    }
-    return (s);
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+	return (s);
 }
