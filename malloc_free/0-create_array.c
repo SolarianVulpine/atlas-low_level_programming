8dc911d88b203c46d8102e94a54230eadd2fbf7b
solarianvulpine <victoria.teale222@gmail.com>
2024-06-12 22:53:24 +0000

    created main.h file and started work on create_array

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+ * create_array - will initialize an array of characters and alocate
+ * memory for it before freeing that memory at the end of the function
+ * 
+ * @size: the size to be allocated
+ * @c: the initializing char
+ * 
+ * Return: Pointer to array
+ */
+
---
+++ malloc_free/main.h
@@ -0,0 +1,6 @@
+#ifndef MAIN.H
+#define MAIN.H
+
+ *create_array(unsigned int size, char c);
+
+ #endif
