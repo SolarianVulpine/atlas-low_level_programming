510165c9bfba8bca31e56134322c2c9a0be90af4
solarianvulpine <victoria.teale222@gmail.com>
2024-06-13 03:09:28 +0000

    betty fixes in str_concat

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -17,25 +17,25 @@ char *str_concat(char *s1, char *s2)
 {
 	char *newstr;
 
-    int i;
+	int i;
 
 	int s1_length = 0;
 
 	int s2_length = 0;
 
-    int new_length = 0;
+	int new_length = 0;
 
 	if (s1 != NULL)
 	{
-        for (s1_length = 0; s1[s1_length]!= '\0'; s1_length++);
-    }
+		for (s1_length = 0; s1[s1_length]!= '\0'; s1_length++);
+	}
 
 	if (s2 != NULL)
 	{
-        for (s2_length = 0; s2[s2_length]!= '\0'; s2_length++);
-    }
+		for (s2_length = 0; s2[s2_length]!= '\0'; s2_length++);
+	}
 
-    new_length = s1_length + s2_length + 1;
+	new_length = s1_length + s2_length + 1;
 
 	newstr = malloc(sizeof(char) * new_length);
 
@@ -43,18 +43,18 @@ char *str_concat(char *s1, char *s2)
 	{
 		return (NULL);
 	}
-    
-    for (i = 0; i < s1_length; i++)
-    {
-        newstr[i] = s1[i];
-    }
 
-    for (i = 0; i < s2_length; i++)
-    {
-        newstr[s1_length + i] = s2[i];
-    }
+	for (i = 0; i < s1_length; i++)
+	{
+		newstr[i] = s1[i];
+	}
 
-    newstr[new_length - 1] = '\0';
+	for (i = 0; i < s2_length; i++)
+	{
+		newstr[s1_length + i] = s2[i];
+	}
+
+	newstr[new_length - 1] = '\0';
 
 	return (newstr);
 
