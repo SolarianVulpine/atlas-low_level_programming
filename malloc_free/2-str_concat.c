65a6ad2de1a44ec6336d97f9613cf5c464bdcceb
solarianvulpine <victoria.teale222@gmail.com>
2024-06-13 02:02:42 +0000

    created 2-main, strncat, and updated main.h

---
+++ malloc_free/2-str_concat.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+
+/**
+* str_concat - will concatentate two strings while also
+* allocating memory to contain the two strings
+*
+* @s1: the first string
+* @s2: the second string
+*
+* Return: a pointer to the new memory space of the strings
+*/
+
+char *str_concat(char *s1, char *s2)
+{
+    
+}
