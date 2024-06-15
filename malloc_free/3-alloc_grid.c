707e9663777ea00f2584ce826599a44f592db449
solarianvulpine <victoria.teale222@gmail.com>
2024-06-15 00:49:39 +0000

    wrote out code for 3-alloc

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -5,6 +5,35 @@
 int **alloc_grid(int width, int height)
 {
     int i, j, k;
+    int **grid;
 
-    
+    if (width <= 0 || height <= 0)
+    {
+        return (NULL);
+    }
+
+    grid = (int **)malloc(width * sizeof(int *));
+
+    if (grid == NULL)
+    {
+        return (NULL);
+    }
+
+    for (i = 0; i < height; i++)
+    {
+        gird[i] = (int *)malloc(height * sizeof(int));
+        
+        if (grid[i] == NULL)
+        {
+            for (j = 0; j < i; j++)
+            {
+                free(grid[j]);
+            }
+            free(grid);
+            
+            return (NULL);
+        }
+    }
+
+    return (grid);
 }
