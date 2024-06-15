c79a8993005a7e42f3aec81570209cc7b7b43a50
solarianvulpine <victoria.teale222@gmail.com>
2024-06-15 00:57:17 +0000

    betty fixes and gird/grid typo

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -2,38 +2,47 @@
 #include <stdlib.h>
 #include "main.h"
 
+/**
+ * alloc_grid - will return a pointer to the 2dimensional array while
+ * also allocating memory space for whatever size that array ends up being
+ *
+ * @width: int for columns
+ * @height: int for rows
+ *
+ * Return: Pointer to grid
+ */
+
 int **alloc_grid(int width, int height)
 {
-    int i, j, k;
-    int **grid;
+	int i, j, k;
+	int **grid;
 
-    if (width <= 0 || height <= 0)
-    {
-        return (NULL);
-    }
+	if (width <= 0 || height <= 0)
+	{
+		return (NULL);
+	}
 
-    grid = (int **)malloc(width * sizeof(int *));
+	grid = (int **)malloc(width * sizeof(int *));
 
-    if (grid == NULL)
-    {
-        return (NULL);
-    }
+	if (grid == NULL)
+	{
+		return (NULL);
+	}
 
-    for (i = 0; i < height; i++)
-    {
-        gird[i] = (int *)malloc(height * sizeof(int));
-        
-        if (grid[i] == NULL)
-        {
-            for (j = 0; j < i; j++)
-            {
-                free(grid[j]);
-            }
-            free(grid);
-            
-            return (NULL);
-        }
-    }
+	for (i = 0; i < height; i++)
+	{
+		grid[i] = (int *)malloc(height * sizeof(int));
+		if (grid[i] == NULL)
+		{
+			for (j = 0; j < i; j++)
+			{
+				free(grid[j]);
+			}
+			free(grid);
 
-    return (grid);
+			return (NULL);
+		}
+	}
+
+	return (grid);
 }
