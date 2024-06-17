d4a98f1bda88fc97b37bbd7a21ed84f7be36c3a5
solarianvulpine <victoria.teale222@gmail.com>
2024-06-17 19:42:36 +0000

    Removed spaces from lines 35 and 44, and changed mallac variable from new_length to n

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -32,7 +32,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
     else if (s1 == NULL)
     {
-        s1 = " ";
+        s1 = "";
     }
 
 	if (s2 != NULL)
@@ -41,12 +41,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
       else if (s2 == NULL)
     {
-        s2 = " ";
+        s2 = "";
     }
 
 	new_length = s1_length + s2_length + 1;
 
-	newstr = malloc(sizeof(char) * new_length);
+	newstr = malloc(sizeof(char) * n);
 
 	if (newstr == NULL)
 	{
