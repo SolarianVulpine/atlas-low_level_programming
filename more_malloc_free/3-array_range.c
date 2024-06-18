040a3a262d2621b06cc3757a48186de9a1e9c5ce
solarianvulpine <victoria.teale222@gmail.com>
2024-06-18 00:36:35 +0000

    betty?

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -4,29 +4,30 @@
 
 /**
  * array_range -
- * 
+ *
  * @min: first int in array
  * @max: last int in array
- * 
+ *
  * Return: pointer to array
  */
 
 int *array_range(int min, int max)
 {
     /**
-     * @counter: counts the number of ints from min to max
-     * @i: the loop iterator
-     * @array: the array of ints being initialized
-     */
+    * @counter: counts the number of ints from min to max
+    * @i: the loop iterator
+    * @array: the array of ints being initialized
+    */
     int counter;
     int i;
     int *array;
 
     /**
-     * failsafe: if min is less than max the function returns NULL\
-     * else the counter is set to calculate the ints
-     * max minus min, for the number of ints, plus 1 to include the min/max as endcaps
-     */
+    * failsafe: if min is less than max the function returns NULL\
+    * else the counter is set to calculate the ints
+    * max minus min, for the number of ints, plus 1 
+    * to include the min/max as endcaps
+    */
     if (min > max)
     {
         return (NULL);
@@ -35,23 +36,23 @@ int *array_range(int min, int max)
     counter = max - min + 1;
 
     /**
-     * utilizes malloc to allocate memory
-     * calculates enough space for 'counter' amount of ints
-     */
+    * utilizes malloc to allocate memory
+    * calculates enough space for 'counter' amount of ints
+    */
     array = malloc(sizeof(int) * counter);
 
     /**
-     * if malloc is unsuccessful return NULL
-     */
+    * if malloc is unsuccessful return NULL
+    */
     if (array == NULL)
     {
         return (NULL);
     }
 
     /**
-     * initializes the array
-     * iterates min each time to go in order
-     */
+    * initializes the array
+    * iterates min each time to go in order
+    */
     for (i = 0; i < counter; i++)
     {
         array[i] = min;
