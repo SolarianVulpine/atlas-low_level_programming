5b316600481be02b3aaf7e9d33718b15f5cec821
solarianvulpine <victoria.teale222@gmail.com>
2024-06-18 00:28:55 +0000

    array range compiles and checks locally, checker time, (I don't know what the new core files are)

---
+++ more_malloc_free/3-array_range.c
@@ -0,0 +1,64 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_range -
+ * 
+ * @min: first int in array
+ * @max: last int in array
+ * 
+ * Return: pointer to array
+ */
+
+int *array_range(int min, int max)
+{
+    /**
+     * @counter: counts the number of ints from min to max
+     * @i: the loop iterator
+     * @array: the array of ints being initialized
+     */
+    int counter;
+    int i;
+    int *array;
+
+    /**
+     * failsafe: if min is less than max the function returns NULL\
+     * else the counter is set to calculate the ints
+     * max minus min, for the number of ints, plus 1 to include the min/max as endcaps
+     */
+    if (min > max)
+    {
+        return (NULL);
+    }
+
+    counter = max - min + 1;
+
+    /**
+     * utilizes malloc to allocate memory
+     * calculates enough space for 'counter' amount of ints
+     */
+    array = malloc(sizeof(int) * counter);
+
+    /**
+     * if malloc is unsuccessful return NULL
+     */
+    if (array == NULL)
+    {
+        return (NULL);
+    }
+
+    /**
+     * initializes the array
+     * min plus 1 ensures that the ints are set in order from min to max
+     */
+    for (i = 0; i < counter; i++)
+    {
+        array[i] = min + 1;
+    }
+
+    /**
+     * returns a pointer to the new array
+     */
+    return (array);
+}
