ffa36a5039c9b1e6256d32920915f9147058163d
solarianvulpine <victoria.teale222@gmail.com>
2024-06-18 01:43:46 +0000

    created 2-main to print file name

---
+++ preprocessor/2-main.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+* main - will print the name of the file it compiles from
+*
+* Return: Zero (success)
+*/
+
+int main(void)
+{
+	printf("%s\n", __FILE__);
+	return (0);
+}
