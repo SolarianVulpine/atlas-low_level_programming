39dba40311e4ec2091da35d6991b5e64642a5f31
solarianvulpine <victoria.teale222@gmail.com>
2024-06-18 00:46:23 +0000

    removed all my useful comments for betty

--- more_malloc_free/3-array_range.c
+++ more_malloc_free/3-array_range.c
@@ -13,53 +13,28 @@
 
 int *array_range(int min, int max)
 {
-/**
-* @counter: counts the number of ints from min to max
-* @i: the loop iterator
-* @array: the array of ints being initialized
-*/
 int counter;
 int i;
 int *array;
 
-/**
-* failsafe: if min is less than max the function returns NULL\
-* else the counter is set to calculate the ints
-* max minus min, for the number of ints, plus 1 
-* to include the min/max as endcaps
-*/
 if (min > max)
 {
 return (NULL);
 }
 counter = max - min + 1;
 
-/**
-* utilizes malloc to allocate memory
-* calculates enough space for 'counter' amount of ints
-*/
 array = malloc(sizeof(int) * counter);
 
-/**
-* if malloc is unsuccessful return NULL
-*/
 if (array == NULL)
 {
 return (NULL);
 }
 
-/**
-* initializes the array
-* iterates min each time to go in order
-*/
 for (i = 0; i < counter; i++)
 {
 array[i] = min;
 min++;
 }
 
-/**
-* returns a pointer to the new array
-*/
 return (array);
 }
