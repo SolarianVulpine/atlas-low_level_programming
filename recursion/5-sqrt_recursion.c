80451e6fd8bafc58086ff7f122dace37b03d3deb
solarianvulpine <victoria.teale222@gmail.com>
2024-06-18 17:51:09 +0000

    saving set up for sqrt_recursion

---
+++ recursion/5-sqrt_recursion.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+/**
+ * _sqrt_recursion - returnd the natural square root of a number
+ * 
+ * @n: the provided number
+ * 
+ * Return: the resulting value
+ */
+
+int find_sqrt(int n, int i)
