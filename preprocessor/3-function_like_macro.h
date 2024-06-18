d8d6f24a30140c164dae479e0868435baee20c22
solarianvulpine <victoria.teale222@gmail.com>
2024-06-18 02:01:14 +0000

    does removing my description appease betty

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,11 +1,6 @@
 #ifndef ABS_H
 #define ABS_H
 
-/**
- * @x: the variable to be absolute valued
- * if x is less than 0; multiply by -1 to become positive
- * else return number as is already positive
- */
 #define ABS(x) (((x) < 0) ? ((x) * -1) : (x))
 
 #endif
