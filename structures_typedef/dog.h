879ba2c1cbb3b3d5cc2dc1970d50e19b7793229a
solarianvulpine <victoria.teale222@gmail.com>
2024-06-21 02:27:50 +0000

    created dog.h

---
+++ structures_typedef/dog.h
@@ -0,0 +1,20 @@
+#ifndef DOG_H
+#define DOG_H
+
+/**
+ * struct dog - structure defining dog data
+ * 
+ * @name: name data of dog
+ * @age: age data of dog
+ * @owner: data of dog's owners
+ *
+ */
+
+struct dog
+{
+    char *name;
+    float age;
+    char *owner;
+};
+
+#endif
