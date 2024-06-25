574cb09ca1521bfa3038ebef360ea712dfbac3f2
solarianvulpine <victoria.teale222@gmail.com>
2024-06-25 02:32:54 +0000

    please let me stay awake long enough

---
+++ variadic_functions/0-sum_them_all.c
@@ -0,0 +1,27 @@
+#include <stdarg.h>
+#include "variadic_functions.h"
+
+/**
+ * sum_them_all - gets sum of parameters
+ * 
+ * @n: num of parameters to add
+ *
+ * Return: sum of parameters
+ */
+
+int sum_them_all(const unsigned int n, ...)
+{
+	va_list args;
+	unsigned int i;
+	int sum;
+
+    va_start(args, n);
+
+	sum = 0;
+	for (i = 0; i < n; i++)
+	{
+		sum += va_arg(args, int);
+	}
+
+	return (sum);
+}
