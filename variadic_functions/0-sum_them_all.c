660780a783e878d0b0106540977edc969aa3f15e
solarianvulpine <victoria.teale222@gmail.com>
2024-06-25 02:39:56 +0000

    betty

--- variadic_functions/0-sum_them_all.c
+++ variadic_functions/0-sum_them_all.c
@@ -2,20 +2,21 @@
 #include "variadic_functions.h"
 
 /**
- * sum_them_all - gets sum of parameters
- * 
- * @n: num of parameters to add
- *
- * Return: sum of parameters
- */
+* sum_them_all - gets sum of parameters
+*
+* @n: num of parameters to add
+*
+* Return: sum of parameters
+*/
 
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list args;
 	unsigned int i;
+
 	int sum;
 
-    va_start(args, n);
+	va_start(args, n);
 
 	sum = 0;
 	for (i = 0; i < n; i++)
