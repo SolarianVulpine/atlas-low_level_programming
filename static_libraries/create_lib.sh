fe3d802beec0360be5c1df968e0b6dce363e4bbf
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-06-27 03:05:27 +0000

    wrote bash script, it seems to have done something

--- static_libraries/create_lib.sh
+++ static_libraries/create_lib.sh
@@ -1,9 +1,7 @@
-!#/bin/bash
-
 #!/bin/bash
 # Create object files from all .c files.
-gcc -c .c
+gcc -c *.c
 #creates the library from .o files
-ar rcs liball.a .o
+ar rcs liball.a *.o
 #clean up .o
-rm .o
+rm *.o
