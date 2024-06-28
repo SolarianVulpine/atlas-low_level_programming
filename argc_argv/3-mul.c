4085879c31cc9ca3b8198f4bb185a5afd3396db6
SolarianVulpine <155486318+SolarianVulpine@users.noreply.github.com>
2024-06-28 11:04:13 +0000

    coded and compiled 3-mul

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - this function will multiply two numbers
+ *
+ * @argc: how many args
+ * @argv: array of arg values
+ *
+ * Return: 0 on success
+ */
+
+int main(int argc, char *argv[])
+{
+int i;
+int k;
+
+if (argc != 3)
+{
+printf("Error\n");
+return (1);
+}
+
+i = atoi(argv[1]);
+k = atoi(argv[2]);
+printf("%d\n", i * k);
+
+return (0);
+}
