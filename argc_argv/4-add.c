563be75a45e4323ee4466d835568c70ad9772741
solarianvulpine <victoria.teale222@gmail.com>
2024-06-29 01:09:23 +0000

    betty coding style

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -13,7 +13,7 @@
 
 int main(int argc, char *argv[])
 {
-	int i,j;
+	int i, j;
 
 	int sum;
 
@@ -23,7 +23,7 @@ int main(int argc, char *argv[])
 		return (0);
 	}
 
-	for(i = 1; i < argc; i++)
+	for (i = 1; i < argc; i++)
 	{
 		for (j = 0; argv[i][j] != '\0'; j++)
 		{
