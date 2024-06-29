994afbba82490bef20f24d1c557fffde8e5f7d1b
solarianvulpine <victoria.teale222@gmail.com>
2024-06-29 01:08:31 +0000

    betty coding style

--- argc_argv/4-add.c
+++ argc_argv/4-add.c
@@ -3,38 +3,39 @@
 #include <ctype.h>
 
 /**
- * main - this function will get the sum of it's parts
- *
- * @argc: how many args
- * @argv: array of arg values
- *
- * Return: 0 on success
- */
+* main - this function will get the sum of it's parts
+*
+* @argc: how many args
+* @argv: array of arg values
+*
+* Return: 0 on success
+*/
 
 int main(int argc, char *argv[])
 {
-    int i,j;
-    int sum;
+	int i,j;
 
-    if (argc == 1)
-    {
-        printf("0\n");
-        return (0);
-    }    
+	int sum;
 
-    for(i = 1; i < argc; i++)
-    {
-        for (j = 0; argv[i][j] != '\0'; j++)
-        {
-            if (!isdigit(argv[i][j]))
-            {
-                printf("Error\n");
-                return (1);
-            }
-        }
-        sum += atoi(argv[i]);
-    }
-    printf("%d\n", sum);
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
 
-    return (0);
+	for(i = 1; i < argc; i++)
+	{
+		for (j = 0; argv[i][j] != '\0'; j++)
+		{
+			if (!isdigit(argv[i][j]))
+			{
+				printf("Error\n");
+				return (1);
+			}
+		}
+		sum += atoi(argv[i]);
+	}
+	printf("%d\n", sum);
+
+	return (0);
 }
