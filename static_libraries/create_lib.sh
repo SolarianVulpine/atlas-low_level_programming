de75c50992bb83d7eda653b09dae6d8911f1cc8b
solarianvulpine <victoria.teale222@gmail.com>
2024-06-29 00:04:46 +0000

    i'm somehow missing files i think

--- static_libraries/create_lib.sh
+++ static_libraries/create_lib.sh
@@ -1,7 +1,21 @@
 #!/bin/bash
-# Create object files from all .c files.
-gcc -c *.c*
-#creates the library from .o files
-ar rcs liball.a *.o*
-#clean up .o
-rm *.o*
+
+# Compile all .c files in the current directory
+for file in *.c; do
+    if [ -f "$file" ]; then
+        # Extract the file name without extension
+        filename=$(basename "$file" .c)
+        # Compile the C file and create an object file with the same name
+        gcc -c "$file" -o "$filename.o"
+        if [ $? -eq 0 ]; then
+            echo "Compiled $file successfully"
+        else
+            echo "Failed to compile $file"
+	    exit 1
+        fi
+    fi
+done
+
+#create library and index
+ar rc liball.a *.o
+ranlib liball.a
