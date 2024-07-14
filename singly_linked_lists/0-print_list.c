acda47c95ae5794381f5a10366b5b7d935bbd1f9
solarianvulpine <victoria.teale222@gmail.com>
2024-07-14 22:04:04 +0000

    filled out header file; wrote out 0-print_lists.c

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -3,14 +3,38 @@
 #include <stdlib.h>
 
 /**
- * print_list - will print the elements of linked list
- * 
- * @h: first element
- * 
- * Return: node count
- */
+* print_list - will print the elements of linked list
+*
+* @h: first element
+*
+* Return: node count
+*/
 
 size_t print_list(const list_t *h)
 {
-    
+	unsigned long count = 0;
+
+	const list_t *current = h;
+
+	while (current != NULL)
+	{
+		if (current->str[current->len - 1] == '\0')
+		{
+			printf("[0] (nil)\n");
+		}
+		else
+		{
+			printf("[%d] ", current->len);
+			for (int i = 0; i < current->len; i++)
+
+			{
+				putchar(current->str[i]);
+			}
+			printf("\n");
+		}
+		current = current->next;
+		current++;
+	}
+
+	return (count);
 }
