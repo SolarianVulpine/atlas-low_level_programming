6b5dad0dbcb75f8b6bfc2c6d6a12d11ea3a30fa7
solarianvulpine <victoria.teale222@gmail.com>
2024-07-15 17:30:25 +0000

    betty style fixes in add node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -16,44 +16,44 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new_element = NULL;
 
-    if (head == NULL)
-    {
-        return (NULL);
-    }
+	if (head == NULL)
+	{
+		return (NULL);
+	}
 
-    new_element = (list_t *)malloc(sizeof(list_t));
+	new_element = (list_t *)malloc(sizeof(list_t));
 
-    if (new_element == NULL)
-    {
-        return (NULL);
-    }
+	if (new_element == NULL)
+	{
+		return (NULL);
+	}
 
-    new_element->str = strdup(str);
+	new_element->str = strdup(str);
 
-    if (!new_element->str) 
-    {
-        free(new_element->str);
-        free(new_element);
-        return NULL;
-    }
+	if (!new_element->str)
+	{
+		free(new_element->str);
+		free(new_element);
+		return (NULL);
+	}
 
-    new_element->len = _strlen(new_element->str);
-    new_element->next = *head;
+	new_element->len = _strlen(new_element->str);
+	new_element->next = *head;
 
-    *head = new_element;
+	*head = new_element;
 
-    return *head;
+	return (*head);
 }
 
 /**
- * _strlen - will return the string length
- *
- * Description:
- *
- * @str: the provided string
- *
- * Return: the length of the string
- */
+* _strlen - will return the string length
+*
+* Description:
+*
+* @str: the provided string
+*
+* Return: the length of the string
+*/
 
 int _strlen(char *str)
 {
