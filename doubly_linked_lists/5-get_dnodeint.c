4528ca371a82411c53cb52bf6c78864fa296a064
solarianvulpine <victoria.teale222@gmail.com>
2024-07-16 04:27:32 +0000

    compiling results are slightly different from greybox example?

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -3,8 +3,9 @@
 #include "lists.h"
 
 /**
-* get_dnodeint_at_index - gets nth node of a doubly linked list
-* @head: head node in list
+* get_dnodeint_at_index - gets node at n
+*
+* @head: head node
 * @index: position of node in list to get
 *
 * Return: address of node at index, NULL if node doesn't exist
@@ -13,21 +14,25 @@
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	dlistint_t *tmp = head;
-	unsigned int nodeCount = 0;
+	unsigned int node_count = 0;
 
-	unsigned int targetIndex = index;
+	/**
+	* unsigned int targetIndex = index;
 
-	if (targetIndex > 0 && tmp != NULL)
+	*
+	*
+	* 	if (targetIndex > 0 && tmp != NULL)
+	*/
+
+	while (tmp != NULL)
 	{
-		while (tmp != NULL)
+		node_count++;
+		if (node_count == index)
 		{
-			nodeCount++;
-			if (nodeCount == targetIndex)
-			{
-				return (tmp);
-			}
-			tmp = tmp->next;
+			return (tmp);
 		}
+        node_count++;
+		tmp = tmp->next;
 	}
 
 	return (NULL);
